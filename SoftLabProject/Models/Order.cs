9d9174a09bd78e0ad2dcebed2ca628ac882f5570
Kiamrs <ph.computer89@yahoo.com>
2016-06-17 17:17:00 +0430

    Project Is Created

---
+++ SoftLabProject/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftLabProject.Models
+{
+    public class Order
+    {
+        public int ID;
+        public DateTime OrderDate;
+        public int CustomerID;
+    }
+}
---
+++ SoftLabProject/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftLabProject.Models
+{
+    public class Product
+    {
+        int id;
+        public string ProductName;
+
+    }
+
+}
